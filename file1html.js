fae59ef9c6167e93927fc0be03171a7da8ef74e0
Tuwane <152686997+Tuwane@users.noreply.github.com>
2025-06-02 14:13:03 +0200

    initial commit

---
+++ file1html.js
@@ -0,0 +1,10 @@
+
+var username = "Mohapi";
+var password = "7745";
+
+
+function Veryfy()
+{
+	var username = document.getElementById("username").value;
+	var username = document.getElementById("password").value;
+} 
